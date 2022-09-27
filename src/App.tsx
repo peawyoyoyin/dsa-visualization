0993a2a180828beacacaf50a79c1171437b6c65f
spdy999 <1a1a1a.262626.0010@gmail.com>
2022-09-27 21:41:03 +0700

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import type { Component } from 'solid-js';
 
 const App: Component = () => {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+      <div>
+        <div class="py-2 px-3 bg-neutral-200 flex justify-between">
+          <span>
+            DSA
+          </span>
+        </div>
+      </div>
   );
 };
 
