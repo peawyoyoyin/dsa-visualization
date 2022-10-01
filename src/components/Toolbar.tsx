acf195b768e438a4e118f911491e50bea636a010
spdy999 <1a1a1a.262626.0010@gmail.com>
2022-10-01 18:35:11 +0700

    finish Toolbar style

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -2,9 +2,17 @@ import type {Component} from 'solid-js';
 
 const Toolbar: Component = () => {
     return (
-        <div>
-            <div>
-                <i class="fa-solid fa-user"></i>
+        <div class="border-solid border-2 border-black max-w-fit m-2">
+            <div class="flex flex-col m-1 justify-items-center items-center">
+                <div>
+                    <i class="fa-solid fa-arrow-pointer"></i>
+                </div>
+                <div>
+                    <i class="fa-regular fa-circle"></i>
+                </div>
+                <div>
+                    <i class="fa-solid fa-play"></i>
+                </div>
             </div>
         </div>
     );
