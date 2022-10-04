5ac3757b9682e26528669dfde256cd19f7cc9c22
spdy999 <1a1a1a.262626.0010@gmail.com>
2022-10-04 18:07:58 +0700

    fixed Toolbar and Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import type { Component } from 'solid-js';
 import MainView from './MainView/MainView';
-import Toolbar from "./components/Toolbar";
+import Toolbar from './components/Toolbar';
 
 const App: Component = () => {
-  return (
-      <div>
-        <div class="py-2 px-3 bg-neutral-200 flex justify-between">
-          <span>
-            DSA
-          </span>
+    return (
+        <div>
+            <div class="fixed w-full py-2 px-3 bg-neutral-200 flex justify-between">
+                <span>DSA</span>
+            </div>
+            <Toolbar />
+            <MainView />
         </div>
-        <Toolbar/>
-        <MainView />
-      </div>
-  );
+    );
 };
 
 export default App;
--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,20 +1,21 @@
-import type {Component} from 'solid-js';
+import type { Component } from 'solid-js';
 
 const Toolbar: Component = () => {
     const onClickSelectTool = () => {
-        alert("Select Tool Clicked!")
-    }
+        alert('Select Tool Clicked!');
+    };
 
     const onClickPointer = () => {
-        alert("Pointer Clicked!")
-    }
+        alert('Pointer Clicked!');
+    };
 
     const onClickNode = () => {
-        alert("Node Clicked!")
-    }
-    const btnStyle = () => "hover:bg-slate-300 px-2 rounded-md transition-colors"
+        alert('Node Clicked!');
+    };
+    const btnStyle = () =>
+        'hover:bg-slate-300 px-2 rounded-md transition-colors';
     return (
-        <div class="border-solid border-2 border-black max-w-fit m-2 p-1">
+        <div class="fixed top-11 left-2 border-solid bg-white border-2 border-black max-w-fit p-1">
             <div class="flex flex-col justify-items-center items-center">
                 <button onClick={onClickSelectTool} class={btnStyle()}>
                     <i class="fa-solid fa-arrow-pointer"></i>
@@ -27,9 +28,7 @@ const Toolbar: Component = () => {
                 </button>
             </div>
         </div>
-
     );
-}
-
+};
 
 export default Toolbar;
